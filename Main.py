ee6ad081ceb2da3d783922f79dc6937eb86d9c22
ilfocus <qiwang0711@gmail.com>
2020-03-26 23:17:27 +0800

    [add] 完成书本信息上传

--- Main.py
+++ Main.py
@@ -30,7 +30,25 @@ def getBookInfoWithId(bookId):
         if bookId == tempId:
             return bookInfo
 
-
+def getBookCatalogWithId(bookId):
+    data = {
+        "method": "sectionlist",
+        "bookid": bookId,
+    }
+    url = "http://www2.uuumeng.com:9090/xingainian/ComicHandle.ashx"
+    response = requests.get(url, params=data)
+    bookCatalog = response.json()
+    return bookCatalog
+def getBookSectionInfoWithSection(section):
+    data = {
+        "method": "picturelist",
+        "sectionid": section,
+    }
+    url = "http://www2.uuumeng.com:9090/xingainian/ComicHandle.ashx"
+    response = requests.get(url, params=data)
+    bookSection = response.json()
+    print("bookSection",bookSection)
+    return bookSection
 # print(getBookInfoWithId(0))
 
 # 6 新概念英语第一册美音版
@@ -49,10 +67,6 @@ def uploadBookInfo():
 
     for key, value in idMap.items() :
         info = getBookInfoWithId(value)
-        # info.pop("BookStateName")
-        # info.pop("BookColorName")
-        # info.pop("BookExceptUpdateDate")
-        # info.pop("FistIndex")
         b.insert(
 			'BookInfo',  # 表名
         	BmobUpdater.increment(
@@ -65,30 +79,56 @@ def uploadBookInfo():
 			    }
         	)
    	 	).jsonData
+    print("上传书本信息成功！！！")
     return
-uploadBookInfo()
-print("上传书本信息成功！！！")
-
-# 获取书本信息
-# print(
-# 	b.insert(
-# 		'Feedback', # 表名
-# 		BmobUpdater.increment(
-# 			"count", # 原子计数key
-# 			2, # 原子计数值
-# 			{ # 额外信息
-# 				"content": "测试python",
-# 				"user": BmobPointer("_User", "xxx"), # Pointer类型
-# 				"date": BmobDate(1545098009351) ## Date类型
-# 			}
-# 		)
-# 	).jsonData # 输出json格式的内容
-# )
-
-# print(
-# 	b.find( # 查找数据库
-# 		"Feedback", # 表名
-# 		BmobQuerier(). # 新建查询逻辑
-# 			addWhereNotExists("user") # user不存在
-# 		).stringData # 输出string格式的内容
-# )
+def uploadBookCatalog():
+    idMap = {"1": 6, "2": 13, "3": 22, "4": 34,
+             "5": 5, "6": 14, "7": 21, "8": 33}
+    for key, value in idMap.items():
+        info = getBookCatalogWithId(value)
+        
+        for value in info:
+            sort = value["Sort"]
+            b.insert(
+                'BookCatalog' + key,  # 表名
+                BmobUpdater.increment(
+                    "count",  # 原子计数key
+                    sort,  # 原子计数值
+                    { # 额外信息
+                        "content": json.dumps(info),
+                        "user": BmobPointer("_User", "xxx"), # Pointer类型
+                        "date": BmobDate(1545098009351) ## Date类型
+			        }
+                )
+            ).jsonData
+    print("上传书本目录成功！！！")   
+    return
+def uplaodBookSectionInfo():
+    idMap = {"1": 6, "2": 13, "3": 22, "4": 34,
+             "5": 5, "6": 14, "7": 21, "8": 33}
+    for key, value in idMap.items():
+        info = getBookCatalogWithId(value)
+        for value in info:
+            SectionID = value["SectionID"]
+            BookID = value["BookID"]
+            sort = value["Sort"]
+            bookSection = getBookSectionInfoWithSection(SectionID)
+            b.insert(
+                'BookSection' + key,  # 表名
+                BmobUpdater.increment(
+                    "count",  # 原子计数key
+                    sort,  # 原子计数值
+                    { # 额外信息
+                        "content": json.dumps(bookSection),
+                        "BookID" : BookID
+			        }
+                )
+            ).jsonData
+    print("上传书本信息成功！！！")   
+    return
+def main():
+    # uploadBookInfo()
+    # uploadBookCatalog()
+    uplaodBookSectionInfo()
+    return
+main()
